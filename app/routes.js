1e2697335ca98dd496bfa030d1741f7d4e68d948
Nikita Luparev <nikita.luparev@gmail.com>
2016-09-03 02:10:13 +0500

    added templates and routes for main pages

--- app/routes.js
+++ app/routes.js
@@ -6,10 +6,44 @@
 * Nick Luparev nikita.luparev@gmail.com
 ------------------------------------------------------------------------------- */
 
+//------------------------ BEGIN MIDDLEWARE  ---------------------
+function isLoggedIn(req, res, next) {
+  if ( req.isAuthenticated() ) return next();
+
+  res.redirect('/');
+}
+//------------------------ END MIDDLEWARE  ---------------------
+
 module.exports = function(app, passport) {
 
   app.get('/', (req, res) => {
-    res.render('index');
+    res.render('index', { message : '' });
   });
+
+  //------------------------ BEGIN SUBMIT SECTION ---------------------
+  app.get('/submit', isLoggedIn, (req, res) => {
+    res.render('submit');
+  });
+
+  app.post('/submit', (req, res) => {
+
+  });
+  //------------------------ END SUBMIT SECTION ---------------------
+
+  //------------------------ BEGIN LOGIN SECTION  ---------------------
+  
+  app.get('/login', (req, res) => {
+    res.render('login');
+  });
+
+  //------------------------ END LOGIN SECTION  ---------------------
+
+  //------------------------ BEGIN SIGNUP SECTION ---------------------
+  app.get('/signup', (req, res) => {
+    res.render('signup');
+  });
+
+  //------------------------ END SIGNUP SECTION ---------------------
+
 
 };
